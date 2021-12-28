6a02c1ee39df3711d9d6cff18f6a931867702ecb
Frank Andrade <fr4nky.develop3r@gmail.com>
2021-12-28 14:02:50 -0600

    first build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/frank/Desktop/inspiral_web/frontend/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/frank/Desktop/inspiral_web/frontend/src/pages/404.js")),
   "component---src-templates-blog-post-js": preferDefault(require("/Users/frank/Desktop/inspiral_web/frontend/src/templates/blogPost.js")),
   "component---src-templates-page-js": preferDefault(require("/Users/frank/Desktop/inspiral_web/frontend/src/templates/page.js"))
