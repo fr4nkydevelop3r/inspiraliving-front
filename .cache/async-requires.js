4c4a0446efd8c8cc5dade29ebbc50b19cf87e5fc
Frank Andrade <fr4nky.develop3r@gmail.com>
2021-12-28 13:33:14 -0600

    first-commit

---
+++ .cache/async-requires.js
@@ -0,0 +1,9 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-templates-page-js": () => import("./../../../src/templates/page.js" /* webpackChunkName: "component---src-templates-page-js" */),
+  "component---src-templates-preview-js": () => import("./../../../src/templates/preview.js" /* webpackChunkName: "component---src-templates-preview-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-404-js": preferDefault(require("/Users/frank/Desktop/inspiral_web/frontend/src/pages/404.js")),
+  "component---src-templates-page-js": preferDefault(require("/Users/frank/Desktop/inspiral_web/frontend/src/templates/page.js")),
+  "component---src-templates-preview-js": preferDefault(require("/Users/frank/Desktop/inspiral_web/frontend/src/templates/preview.js"))
+}
+
